4d2a8785e9cb68c08163adcc4db2c22025e82913
A90G <104040612+A90G@users.noreply.github.com>
2022-05-24 15:59:55 -0300

    commit 3 new task

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+/*• Desarrolle un algoritmo que
+permita cargar alumnos y sus
+notas en los tres trimestres
+• Se debe permitir obtener el
+promedio anual (es decir, de sus
+tres notas) de un alumno
+(ingresado por el usuario)
+• Luego de resolverlo, pensar en
+aprovechar métodos y discutir
+como representar la información*/
